14ddfcfadf4038a01b8117c552c2793156555926
Abdelwahab <abdelwahab.chibah@dataimpact.io>
2021-03-05 15:48:37 +0100

    library configuration

--- setup.py
+++ setup.py
@@ -2,13 +2,9 @@ from setuptools import find_packages, setup
 
 setup(
     name='mgb',
-    packages=find_packages(include=['mypythonlib']),
+    packages=find_packages(),
     version='0.1.0',
-    description='Mining Groups Behavior',
+    description='Mining groups behavior',
     author='Abdelouahab Chibah',
     license='',
-    install_requires=[],
-    setup_requires=['pytest-runner'],
-    tests_require=['pytest==4.4.1'],
-    test_suite='tests',
 )
